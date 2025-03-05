56b43d98c2b2f1fd948d091fbfdd0e4857211905
Tiago Barros <tbarros1337@hotmail.com>
2025-03-05 10:09:26 -0300

    Add Croct theme (#3)

---
+++ .storybook/lib/theme/croct.ts
@@ -0,0 +1,16 @@
+import {create} from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Croct',
+    brandUrl: 'https://croct.com',
+    brandImage: 'logo-horizontal.svg',
+    colorPrimary: '#4DD684',
+    colorSecondary: '#4DD684',
+    appBg: '#F1F4F5',
+    textColor: '#0F1010',
+    textInverseColor: '#ffffffe6',
+    barTextColor: '#616871',
+    barSelectedColor: '#4DD684',
+    inputTextColor: '#0F1010',
+});
