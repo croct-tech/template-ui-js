1d722ce925b54ecc0a8c76505b0c0ca331485d54
Marcos Passos <marcos@croct.com>
2025-03-06 07:48:44 -0400

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import {defineConfig} from 'vite';
 import {externalizeDeps} from 'vite-plugin-externalize-deps'
-
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-    plugins: [react(), externalizeDeps()],
+    plugins: [react(), externalizeDeps(), dts({
+        exclude: ['**/*.stories.{ts,tsx}'],
+    })],
     build: {
         outDir: 'build',
         lib: {
             entry: [
                 'src/react/index.ts',
-                'src/web/index.ts'
+                'src/web/index.ts',
+                'src/web/registry.ts',
             ],
             formats: ['es'],
         },
