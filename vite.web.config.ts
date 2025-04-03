5d7c1f0b0bbb28a0ff722a0906569c9283688c6a
Marcos Passos <marcos@croct.com>
2025-04-03 11:52:59 -0300

    Configure build

--- vite.web.config.ts
+++ vite.web.config.ts
@@ -1,8 +1,9 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {fixImportsPlugin} from "esbuild-fix-imports-plugin";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), fixImportsPlugin()],
     define: {
         'process.env': {}
     },
