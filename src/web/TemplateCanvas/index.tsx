474ae1f0bd6174be2b323b6ee182b68650bbdb94
Marcos Passos <marcos@croct.com>
2025-05-16 10:14:45 -0300

    Fix flag

--- src/web/TemplateCanvas/index.tsx
+++ src/web/TemplateCanvas/index.tsx
@@ -24,7 +24,7 @@ export const TemplateCanvas = r2wc(
             fullScreen: 'boolean',
             minHeight: 'string',
             portal: 'boolean',
-            frame: 'string',
+            frame: 'boolean',
         },
     },
 );
